55783a72c8f5d948d946eb8563965b0f71dfd5c9
Alkanthor <rogls@seznam.cz>
2017-10-23 17:59:06 +0200

    added rotate animation and multiple models

---
+++ Assets/Scripts/RotateScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private bool animate;
+
+
+    void Start()
+    {
+        animate = false;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            animate = !animate;
+            animator.SetBool("Animate",animate);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/SculptureScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SculptureScript : MonoBehaviour
+{
+
+    private GameObject[] sculptures;
+    private int activeIndex;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    activeIndex = 0;
+        sculptures = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            sculptures[i] = transform.GetChild(i).gameObject;
+            if (i == activeIndex)
+                sculptures[i].SetActive(true);
+           else
+                sculptures[i].SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            activeIndex++;
+            activeIndex = mod(activeIndex, transform.childCount);
+            ChangeModel();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            activeIndex--;
+            activeIndex = mod(activeIndex,transform.childCount);
+            ChangeModel();
+        }
+    }
+
+    private void ChangeModel()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (i == activeIndex)
+                sculptures[i].SetActive(true);
+            else
+                sculptures[i].SetActive(false);
+        }
+    }
+
+    private int mod(int x, int m)
+    {
+        return (x % m + m) % m;
+    }
+}

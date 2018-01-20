5a8b83c5248c93142ebf69b6e5e3d993c38f50cc
Alkanthor <rogls@seznam.cz>
2018-01-20 16:04:46 +0100

    tweaked scenes, basic gallery scene

--- Assets/Scripts/SculptureScript.cs
+++ Assets/Scripts/SculptureScript.cs
@@ -1,26 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SculptureScript : MonoBehaviour
 {
 
     private GameObject[] sculptures;
+    private List<GameObject> balls;
+    private Vector3[] origins;
     private int activeIndex;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    activeIndex = 0;
+    // Use this for initialization
+    void Start()
+    {
+        activeIndex = 0;
         sculptures = new GameObject[transform.childCount];
         for (int i = 0; i < transform.childCount; i++)
         {
             sculptures[i] = transform.GetChild(i).gameObject;
             if (i == activeIndex)
                 sculptures[i].SetActive(true);
-           else
+            else
                 sculptures[i].SetActive(false);
         }
+        SetBallsOrigin();
+
     }
 
     void Update()
@@ -34,7 +39,7 @@ public class SculptureScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.M))
         {
             activeIndex--;
-            activeIndex = mod(activeIndex,transform.childCount);
+            activeIndex = mod(activeIndex, transform.childCount);
             ChangeModel();
         }
     }
@@ -48,10 +53,30 @@ public class SculptureScript : MonoBehaviour
             else
                 sculptures[i].SetActive(false);
         }
+        ResetBalls();
     }
 
     private int mod(int x, int m)
     {
         return (x % m + m) % m;
+    }
+
+    private void SetBallsOrigin()
+    {
+        balls = new List<GameObject>(Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name.Contains("Ball")));
+        origins = new Vector3[balls.Count];
+        for (int i = 0; i < balls.Count; i++)
+        {
+            origins[i] = balls[i].transform.localPosition;
+        }
+    }
+
+
+    private void ResetBalls()
+    {
+        for (int i = 0; i < balls.Count; i++)
+        {
+            balls[i].transform.localPosition = origins[i];
+        }
     }
 }

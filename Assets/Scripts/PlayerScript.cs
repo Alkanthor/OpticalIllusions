5d01891bfe92672fde674e5b33b6edbeb1b0680c
Alkanthor <rogls@seznam.cz>
2018-02-19 20:38:09 +0100

    change models cotroller

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public SculptureScript sculptureScript;
+
+
+    // Use this for initialization
+    void Start () {
+        sculptureScript = GameObject.Find("Sculpture").GetComponent<SculptureScript>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll > 0)
+            sculptureScript.NextModel();
+        if (scroll < 0)
+            sculptureScript.PreviousModel();
+    }
+}

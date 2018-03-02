ccbf9ac07e4bcc43148acc92a3b909e5974b0188
Alkanthor <rogls@seznam.cz>
2018-03-02 20:08:15 +0100

    fps controller for all scenes

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
+    public Animator animator;
+
+    public LightsScript lightsScript;
+
+    private bool animate;
+
     public SculptureScript sculptureScript;
 
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
+        animate = false;
         sculptureScript = GameObject.Find("Sculpture").GetComponent<SculptureScript>();
+        animator = GameObject.Find("Sculpture").GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            animate = !animate;
+            animator.SetBool("Animate", animate);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            lightsScript.TurnLights();
+        }
         var scroll = Input.GetAxis("Mouse ScrollWheel");
         if (scroll > 0)
             sculptureScript.NextModel();
